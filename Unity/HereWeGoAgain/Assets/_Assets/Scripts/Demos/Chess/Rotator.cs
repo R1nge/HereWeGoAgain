2f7942ddc03a61c310f1eb36c3b28b3f6dbe9c10
R1nge <r1ngegamedev@gmail.com>
2024-02-20 00:23:43 +0300

    Rotate, scale

---
+++ Unity/HereWeGoAgain/Assets/_Assets/Scripts/Demos/Chess/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace _Assets.Scripts.Demos.Chess
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0))
+            {
+                var rotation = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * speed, transform.up);
+                transform.rotation = rotation * transform.rotation;
+            }
+        }
+    }
+}
---
+++ Unity/HereWeGoAgain/Assets/_Assets/Scripts/Demos/Chess/Scaler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Assets.Scripts.Demos.Chess
+{
+    public class Scaler : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private float maxScale = 1.4f;
+        [SerializeField] private float minScale = .2f;
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0))
+            {
+                float scaleFactor = Mathf.Pow(2f, Input.GetAxis("Mouse X") * speed);
+
+                float newX = Mathf.Clamp(transform.localScale.x * scaleFactor, minScale, maxScale);
+                float newZ = Mathf.Clamp(transform.localScale.z * scaleFactor, minScale, maxScale);
+
+                transform.localScale = new Vector3(
+                    newX,
+                    transform.localScale.y,
+                    newZ);
+            }
+        }
+    }
+}
