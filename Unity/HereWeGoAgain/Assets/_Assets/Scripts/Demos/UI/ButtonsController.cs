60aa254760bc072f5d57580a5d7aa78a79fc3c8e
R1nge <r1ngegamedev@gmail.com>
2024-02-19 22:57:41 +0300

    5th task

---
+++ Unity/HereWeGoAgain/Assets/_Assets/Scripts/Demos/UI/ButtonsController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Assets.Scripts.Demos.UI
+{
+    public class ButtonsController : MonoBehaviour
+    {
+        [SerializeField] private ButtonsView buttonsView;
+        private float _timer;
+        private CoroutineType _currentCoroutine;
+        private bool _started;
+
+        private void Awake()
+        {
+            buttonsView.OnClick += ChangeState;
+            Stop();
+            ResetTimer();
+        }
+
+        private void ChangeState()
+        {
+            if (_started)
+            {
+                Stop();
+            }
+            else
+            {
+                StartCoroutine(Coroutine1());
+                StartCoroutine(Coroutine2());
+                buttonsView.SetButtonText("Stop");
+                PickRandom();
+            }
+            
+            _started = !_started;
+        }
+
+        private void Stop()
+        {
+            buttonsView.SetText1("0");
+            buttonsView.SetText2("0");
+            buttonsView.SetButtonText("Start");
+            buttonsView.SetIndicator1Color(Color.yellow);
+            buttonsView.SetIndicator2Color(Color.yellow);
+            ResetTimer();
+        }
+
+        private IEnumerator Coroutine1()
+        {
+            Debug.Log("First coroutine started", this);
+            yield break;
+        }
+
+        private IEnumerator Coroutine2()
+        {
+            Debug.Log("Second coroutine started", this);
+            yield break;
+        }
+
+        private void Update()
+        {
+            if (_started)
+            {
+                if (_timer > 0)
+                {
+                    _timer -= Time.deltaTime;
+                    SelectCoroutine();
+                }
+                else
+                {
+                    SwapCoroutines();
+                }
+            }
+        }
+
+        private void SelectCoroutine()
+        {
+            switch (_currentCoroutine)
+            {
+                case CoroutineType.First:
+                    buttonsView.SetText1(_timer.ToString(CultureInfo.InvariantCulture));
+                    buttonsView.SetIndicator1Color(Color.red);
+                    buttonsView.SetIndicator2Color(Color.yellow);
+                    Log();
+                    break;
+                case CoroutineType.Second:
+                    buttonsView.SetText2(_timer.ToString(CultureInfo.InvariantCulture));
+                    buttonsView.SetIndicator1Color(Color.yellow);
+                    buttonsView.SetIndicator2Color(Color.red);
+                    Log();
+                    break;
+            }
+        }
+
+        private void SwapCoroutines()
+        {
+            PickRandom();
+            switch (_currentCoroutine)
+            {
+                case CoroutineType.First:
+                    buttonsView.SetText1(0.ToString());
+                    buttonsView.SetText2(0.ToString());
+                    buttonsView.SetIndicator1Color(Color.red);
+                    buttonsView.SetIndicator2Color(Color.yellow);
+                    ResetTimer();
+                    StartCoroutine(Coroutine2());
+                    Log();
+                    break;
+                case CoroutineType.Second:
+                    buttonsView.SetText1(0.ToString());
+                    buttonsView.SetText2(0.ToString());
+                    buttonsView.SetIndicator1Color(Color.yellow);
+                    buttonsView.SetIndicator2Color(Color.red);
+                    ResetTimer();
+                    StartCoroutine(Coroutine1());
+                    Log();
+                    break;
+            }
+        }
+
+        private void PickRandom() => _currentCoroutine = (CoroutineType)Random.Range(0, Enum.GetNames(typeof(CoroutineType)).Length);
+
+        private void ResetTimer() => _timer = Random.Range(10, 21);
+
+        private void Log()
+        {
+            switch (_currentCoroutine)
+            {
+                case CoroutineType.First:
+                    Debug.Log("First coroutine is leading", this);
+                    break;
+                case CoroutineType.Second:
+                    Debug.Log("Second coroutine is leading", this);
+                    break;
+            }
+        }
+
+        private enum CoroutineType : byte
+        {
+            First = 0,
+            Second = 1
+        }
+
+        private void OnDestroy() => buttonsView.OnClick -= ChangeState;
+    }
+}
---
+++ Unity/HereWeGoAgain/Assets/_Assets/Scripts/Demos/UI/ButtonsView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Assets.Scripts.Demos.UI
+{
+    public class ButtonsView : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private Image indicator1, indicator2;
+        [SerializeField] private TextMeshProUGUI text1, text2;
+        public event Action OnClick;
+
+        public void SetButtonText(string text) => button.GetComponentInChildren<TextMeshProUGUI>().text = text;
+        public void SetIndicator1Color(Color value) => indicator1.color = value;
+        public void SetIndicator2Color(Color value) => indicator2.color = value;
+        public void SetText1(string value) => text1.text = value;
+        public void SetText2(string value) => text2.text = value;
+
+        private void Awake() => button.onClick.AddListener(Click);
+
+        private void Click() => OnClick?.Invoke();
+
+        private void OnDestroy() => button.onClick.RemoveAllListeners();
+    }
+}
