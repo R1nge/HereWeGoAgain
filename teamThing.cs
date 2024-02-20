39580e2f20b326169c6e04536033b930c2461ed0
R1nge <r1ngegamedev@gmail.com>
2024-02-20 12:53:30 +0300

    Update teamThing.cs

--- teamThing.cs
+++ teamThing.cs
@@ -1,25 +1,32 @@
-public static void Main(string[] args) {
-  var random = new Random();
-  var teams = new Team[20];
-  for (int i = 0; i < teams.Length; i++) {
-    teams[i] = new Team(random.Next(100));
-    Console.WriteLine($"Team: {i} Score: {teams[i].Score}");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class HelloWorld {
+  static void Main() {
+    var random = new Random();
+    var teams = new Team[20];
+    for (int i = 0; i < teams.Length; i++) {
+      teams[i] = new Team(random.Next(100));
+      Console.WriteLine($"Team: {i} Score: {teams[i].Score}");
+    }
+
+    Console.WriteLine("");
+
+    var sorted = teams.OrderByDescending(t => t.Score).ToArray();
+    var sum = 0;
+    for (int i = 0; i < 3; i++) {
+      Console.WriteLine($"Team: {i} Score: {sorted[i].Score}");
+      sum += sorted[i].Score;
+    }
+
+    Console.WriteLine(sum);
   }
 
-  Console.WriteLine("");
+  public struct Team {
+    public int Score;
 
-  var sorted = teams.OrderByDescending(t => t.Score).ToArray();
-  var sum = 0;
-  for (int i = 0; i < 3; i++) {
-    Console.WriteLine($"Team: {i} Score: {sorted[i].Score}");
-    sum += sorted[i].Score;
+    public Team(int score) {
+      Score = score;
+    }
   }
-
-  Console.WriteLine(sum);
-}
-
-public struct Team {
-  public int Score;
-
-  public Team(int score) { Score = score; }
 }
